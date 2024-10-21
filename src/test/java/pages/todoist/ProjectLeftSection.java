15176906c59f8fdd7a8104dadaece485a6112aaf
RebecaNavarro <becanavarro2003@gmail.com>
2024-10-21 16:37:18 -0400

    commit tarea con la prueba login en todoist, pero falta el create project

--- src/test/java/pages/todoist/ProjectLeftSection.java
+++ src/test/java/pages/todoist/ProjectLeftSection.java
@@ -13,9 +13,5 @@ public class ProjectLeftSection {
 
     public Button anadirButton = new Button(By.xpath("//*[@id=':r5b:']/form/div[2]/footer/div/div/button[2]"));
 
-    public Label getProjectLabel(String name) {
-        Label projectLabel = new Label(By.xpath("//div[@data-testid='virtuoso-item-list' and last()]//div//div//div/li//div//div//a//div//span[contains(text(),'" + name + "')]"));
-        return projectLabel;
-    }
 
 }
