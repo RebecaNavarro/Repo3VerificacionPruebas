0e25f566723ac9f2e68b75e3cfe50af3a6776f74
RebecaNavarro <becanavarro2003@gmail.com>
2024-10-21 11:36:47 -0400

    commit con cambios que no debo subir

--- src/test/java/basic/UpdateTest.java
+++ src/test/java/basic/UpdateTest.java
@@ -3,13 +3,13 @@ package basic;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
+import java.util.Date;
 
 public class UpdateTest {
+
 
     WebDriver chrome;
     @BeforeEach
@@ -19,7 +19,6 @@ public class UpdateTest {
         chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         chrome.manage().window().maximize();
         chrome.get("https://todo.ly/");
-
     }
 
     @AfterEach
@@ -28,42 +27,47 @@ public class UpdateTest {
     }
 
     @Test
-    @DisplayName("Verify update project test is successfully")
-    public void verifyAddNewProject() throws InterruptedException {
+    @DisplayName("Verify create update project")
+    public void verifyCreateUpdateProject() throws InterruptedException {
         chrome.findElement(By.xpath("//img[@src=\"/Images/design/pagelogin.png\"]")).click();
         chrome.findElement(By.id("ctl00_MainContent_LoginControl1_TextBoxEmail")).sendKeys("selenium123@123.com");
         chrome.findElement(By.id("ctl00_MainContent_LoginControl1_TextBoxPassword")).sendKeys("12345");
         chrome.findElement(By.id("ctl00_MainContent_LoginControl1_ButtonLogin")).click();
-
         // verification
         Assertions.assertTrue(chrome.findElements(By.id("ctl00_HeaderTopControl1_LinkButtonLogout")).size() ==1,
                 "ERROR! nose pudo iniciar sesion");
 
-        Thread.sleep(3000);
-
-        chrome.findElement(By.className("AddProjectLiDiv")).click();
-        chrome.findElement(By.id("NewProjNameInput")).sendKeys("Para actualizar");
+        // create project
+        // click add new project
+        String nameProject = "UPB"+new Date().getTime();
+        chrome.findElement(By.xpath("//td[text()='Add New Project']")).click();
+        // set project name
+        chrome.findElement(By.id("NewProjNameInput")).sendKeys(nameProject);
+        // click add
         chrome.findElement(By.id("NewProjNameButton")).click();
 
-        Thread.sleep(2000);
+        // verification
+        String expectedResult=nameProject;
+        String actualResult=chrome.findElement(By.xpath("//li[last()]//td[text()='"+nameProject+"'] ")).getText();
+        Assertions.assertEquals(expectedResult,actualResult,"ERROR the project was not created successfully");
 
-        Assertions.assertTrue(chrome.findElement(By.className("CurrentProjectTitle")).getText().contains("Para actualizar"),
-                "ERROR! No se pudo crear el proyecto");
+        // update project
+        //select project created
+        chrome.findElement(By.xpath("//li[last()]//td[text()='"+nameProject+"'] ")).click();
+        // click menu icon
+        chrome.findElement(By.xpath("//div[@style=\"display: block;\"]/img")).click();
+        // click  [edit]
+        chrome.findElement(By.xpath("//ul[contains(@style,\"block;\")]//a[text()='Edit']")).click();
+        // set new project name
+        String nameProjectUpdated = "BOL"+new Date().getTime();
+        chrome.findElement(By.xpath("//input[@id='ItemEditTextbox' and @style]")).clear();
+        chrome.findElement(By.xpath("//input[@id='ItemEditTextbox' and @style]")).sendKeys(nameProjectUpdated);
+        // click  [save] icon
+        chrome.findElement(By.xpath("//img[@id=\"ItemEditSubmit\" and @itemid]")).click();
 
-//        chrome.findElement(By.xpath("//img[@src=\"/Images/dropdown.png\"]")).click();
-//        chrome.findElement(By.xpath("//li[@class='edit']/a")).click();
-//
-//        Thread.sleep(2000);
-//
-//        chrome.findElement(By.id("ItemEditTextbox")).clear();
-//        chrome.findElement(By.id("ItemEditTextbox")).sendKeys("Actualizado");
-//
-//        chrome.findElement(By.xpath("//img[@title='Save']")).click();
-//
-//        Thread.sleep(2000);
-//
-//        Assertions.assertTrue(chrome.findElement(By.className("CurrentProjectTitle")).getText().contains("Actualizado"),
-//                "ERROR! No se pudo actualizar el nombre del proyecto");
-
+        // verification
+        expectedResult=nameProjectUpdated;
+        actualResult=chrome.findElement(By.xpath("//li[last()]//td[text()='"+nameProjectUpdated+"'] ")).getText();
+        Assertions.assertEquals(expectedResult,actualResult,"ERROR the project was not created successfully");
     }
 }
